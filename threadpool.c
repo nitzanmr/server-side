364d9f7c6d38f2a7c52663d15345d67bd107d261
nitzanmr <81086233+nitzanmr@users.noreply.github.com>
2022-12-25 09:06:17 +0000

    wrote the create threadpool function.

--- threadpool.c
+++ threadpool.c
@@ -1,9 +1,13 @@
-struct thread_pool{
-    int number_of_threads;/*max size of threads allowed*/
+#include <pthread.h>
+#include <stdio.h>
+
+struct {
+    int number_of_threads;/*number of active threads <= max size of threads allowed*/
     int qsize;/*the current size of the queue*/
+    pthread_t* threads;
     work_t qhead;
     work_t qtail;
-    pthread_mutex qlock;/*mutex lock for critical portion*/
+    pthread_mutex_t qlock;/*mutex lock for critical portion*/
     int q_not_empty;/*flags*/
     int shutdown;
     int dont_accept;
@@ -14,4 +18,24 @@ struct thread_pool{
         void* args;
         struct work_t* next;
     }typedef work_t;
+
+void init_threadpool(int max_number_of_threads,thread_pool new_threadpool){
+    /*this function initialize the threadpool to it defult values*/
+    new_threadpool.number_of_threads = 0;
+    new_threadpool.qsize = 0;
+    new_threadpool.threads = NULL;
+    new_threadpool.dont_accept=0;
+    new_threadpool.shutdown = 0;
+    new_threadpool.q_not_empty = 0;
+    new_threadpool.qhead = NULL;
+    new_threadpool.qtail = NULL;
+}   
+void create_threadpool(int max_number_of_threads,thread_pool new_threadpool){
+    /*creates the threadpool and asign its purpuse to it.*/
+    init_threadpool(max_number_of_threads,new_threadpool);
+    for (int i = 0; i <max_number_of_threads ;i++){
+        pthread_create(new_threadpool.threads[i],NULL,new_threadpool.qhead->routine,new_threadpool.qhead->args);
+        
+    }
     
+}
