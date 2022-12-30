9f10f990b592e08155702b0ba2665e0bd2594a30
nitzanmr <81086233+nitzanmr@users.noreply.github.com>
2022-12-30 06:10:59 +0000

    checked if it works with more then 5 threads.

--- main.c
+++ main.c
@@ -3,8 +3,9 @@
 #include "malloc.h"
 #include "stdlib.h"
 int job1(void*);
+int job2(void*);
 int main(){
-    int size_pool = 5;
+    int size_pool = 20;
     threadpool* new_threadpool = create_threadpool(size_pool);
     if(new_threadpool==NULL){
         perror("create error");
@@ -12,11 +13,20 @@ int main(){
     }
     for (int i = 0; i < size_pool; i++)
     {
-        dispatch(new_threadpool,(dispatch_fn)job1,(void*)i);
+        if(i%2){
+            dispatch(new_threadpool,(dispatch_fn)job2,(void*)i);
+        }
+        else{
+            dispatch(new_threadpool,(dispatch_fn)job1,(void*)i);
+        }
     }
     destroy_threadpool(new_threadpool);
 }
 int job1(void* number_of_thread){
     printf("\npthread number: %d\n",number_of_thread);
+    return 0;
+}
+int job2(void* number_of_thread){
+    printf("\npthread number: %d\n ",number_of_thread);
     return 0;
 }
